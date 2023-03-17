1d7356de12235b41249835fc66f82e48f03a3692
Daniel <gendik38@gmail.com>
2023-03-17 16:35:59 +0100

    edit layout for relevance filter

--- src/globalComponents/ShopLayout/ShopLayout.tsx
+++ src/globalComponents/ShopLayout/ShopLayout.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import FilterSectionDesktop from "./FilterSectionDesktop"
 import FilterSectionTouch from "./FilterSectionTouch"
 import { Link } from "react-router-dom"
@@ -18,21 +18,21 @@ interface Props {
 }
 
 const ShopLayout: React.FC<Props> = ({ category, pageTitle }) => {
-  const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState("")
 
   const specificCategory = products.filter(
     (product: Item) => product.category === category
   )
 
   const sortedList = specificCategory.slice().sort((a, b) => {
-    if (selectedValue === 'price-max') {
-      return b.price - a.price;
-    } else if (selectedValue === 'price-min') {
-      return a.price - b.price;
+    if (selectedValue === "price-max") {
+      return b.price - a.price
+    } else if (selectedValue === "price-min") {
+      return a.price - b.price
     } else {
-      return 0; // keine Änderung
+      return 0 // keine Änderung
     }
-  });
+  })
 
   const productList = sortedList.map((product: Item) => (
     <Link to={`/items/${product.id}`}>
@@ -77,14 +77,23 @@ const ShopLayout: React.FC<Props> = ({ category, pageTitle }) => {
       // data-aos-once="false"
       // data-aos-anchor-placement="top-center"
       >
+        <div className="headline flex flex-col text-center">
+          <h1 className="text-white font-bold text-4xl lg:hidden">
+            {pageTitle}
+          </h1>
+          <h3 className="text-white lg:hidden font-normal text-lg">
+            {productList.length} items
+          </h3>
+        </div>
         <div
           className="relevance-filter 
-    hidden lg:flex justify-end mr-10"
+     lg:flex justify-end mr-10"
         >
-          <form className="outline-none">
-            <select 
-            className="px-4 py-3 rounded outline-none border-none text-white bg-black cursor-pointer font-medium"
-            onChange={(e) => setSelectedValue(e.target.value)}>
+          <form className=" border-white border-2 w-1/2  lg:w-auto">
+            <select
+              className="px-4 py-3 rounded outline-none border-none text-white bg-black cursor-pointer font-semibold"
+              onChange={(e) => setSelectedValue(e.target.value)}
+            >
               <option value="relevanz">Relevanz</option>
               <option value="price-max">Preis (absteigend)</option>
               <option value="price-min">Preis (aufsteigend)</option>
@@ -94,13 +103,6 @@ const ShopLayout: React.FC<Props> = ({ category, pageTitle }) => {
 
         <div className="shop flex flex-col lg:flex-row bg-black">
           <div className="flex flex-col items-center lg:w-1/3 gap-4">
-            <h1 className="text-white font-bold text-4xl lg:hidden">
-              {pageTitle}
-            </h1>
-            <h3 className="text-white lg:hidden font-normal text-lg">
-              {productList.length} items
-            </h3>
-
             <FilterSectionDesktop />
           </div>
 
@@ -110,8 +112,6 @@ const ShopLayout: React.FC<Props> = ({ category, pageTitle }) => {
        text-center  w-11/12 ml-[2%] mr-[2%] mt-10 
        lg:w-2/3 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2"
           >
-            <FilterSectionTouch />
-
             {productList}
           </div>
         </div>
